6024712c1f0ce0912ae13c72f3f4023bc3e4e0d3
Simon <bardrabbit@gmail.com>
2020-09-20 12:36:37 -0400

    set up the toggle play function and change icon

---
+++ script.js
@@ -0,0 +1,46 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const pause = document.getElementById('pause');
+const progress = document.getElementById('progress');
+
+//Play Pause video and update play pause icon
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+//Update progress and timestamp, set up video to progress
+function updateProgress() {
+  return true;
+}
+
+function setVideoProgress() {
+  return true;
+}
+
+//stop video
+function stopVideo() {
+  return true;
+}
+
+//Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
